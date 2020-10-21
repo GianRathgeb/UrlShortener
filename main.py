47ab245efc4efffb8a9fb3b5deab61933257a3d4
GianRathgeb <gian.rathgeb@bbz-sh.ch>
2020-10-21 08:39:57 +0200

    more added

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def mainPage():
     return 'Hello World'
+
+givenUrl = ''
+
+@app.before_request
+def getUrl():
+    print('request')
+    print(request.args.getlist)
